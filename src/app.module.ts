9231cd7ca277eb444cf9eaf87d2546845b063026
Artyom Aristov <34972991+obsequey@users.noreply.github.com>
2021-09-03 19:06:33 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { UserModule } from "./user/user.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "front")
+    }),
+    UserModule
+  ],
+  controllers: [],
+  providers: []
+})
+export class AppModule {}
---
+++ src/user/user.entity.ts
@@ -0,0 +1,9 @@
+export class User {
+  constructor(id: number, username: string) {
+    this.id = id;
+    this.username = username;
+  }
+
+  id: number;
+  username: string;
+}
